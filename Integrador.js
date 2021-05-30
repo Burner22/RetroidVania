d407cb26ece5f609db34eaecf8c39de613b73997
Acevedo Eze <ezeelkpo2016@gmail.com>
2021-05-30 12:25:06 -0300

    Javascript terminado

--- Integrador.js
+++ Integrador.js
@@ -4,22 +4,20 @@ function test(){
     const nombre = document.getElementById("fnick").value;
     const mail = document.getElementById("fmail").value;
     const juego = document.getElementById("fjuego").value;
+    var errores = [];
+    var aux = -1;
 
     if(!nombre.match(/^[A-Z a-z 0-9\s]+$/) || nombre.length<3){
-        alert("Nick Incorrecto!");
-        document.getElementById("fnick").focus();       
-        return false;
-           
+        aux++;
+        errores[aux] = "Nick Incorrecto! \n";                 
     }
     if(!mail.match(/^([A-Z a-z 0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/) || mail.length < 15){
-        alert("Mail Incorrecto!");
-        document.getElementById("fmail").focus();
-        return false;
+        aux++;
+        errores[aux] = "Mail Incorrecto! \n";     
     } 
     if(!juego.match(/^[A-Z a-z 0-9\s]+$/)){
-        alert("Ingrese algun juego valido!");
-        document.getElementById("fjuego").focus();
-        return false;
+        aux++;
+        errores[aux] = "Ingrese algun juego valido! \n";
     }
     
     const formulario = document.getElementById('nose');
@@ -32,8 +30,8 @@ function test(){
         }
     }  
     if(!c){
-        alert(`Seleccione su consola ${nombre}!`);
-        return false;
+        aux++;
+        errores[aux] = "Seleccione su consola! \n";
     }
     
     const formulario2 = document.getElementById('nose');
@@ -46,35 +44,46 @@ function test(){
         }
     }  
     if(!c){
-        alert(`Seleccione su generacion ${nombre}!`);
-        return false;
-    }   
+        aux++;
+        errores[aux] = "Seleccione su generacion! \n";
+    }  
+    
+    if (aux>0){
+        alert(errores);
+        validado = false;
+    }
+    else{
+        confirm("Desea enviar su formulario?")
+    }
     return validado;
 }
-  
+    
+var slideIndex = 0;
+showSlides(); 
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000);
+}
+
     var recoIndex = 1;
-    recomend(recoIndex);
+    showSlides(recoIndex);
     
     function plus(n) {
-      recomend(recoIndex += n);
+        showSlides(recoIndex += n);
     }
     
     function juegoactual(n) {
-      recomend(recoIndex = n);
-    }
-    
-    function recomend(n) {
-      var i;
-      var jue = document.getElementsByClassName("aka");
-      var sig = document.getElementsByClassName("dol");
-      if (n > jue.length) {recoIndex = 1}    
-      if (n < 1) {recoIndex = jue.length}
-      for (i = 0; i < jue.length; i++) {
-        jue[i].style.display = "none";  
-      }
-      for (i = 0; i < sig.length; i++) {
-        sig[i].className = sig[i].className.replace(" active", "");
-      }
-      jue[recoIndex-1].style.display = "block";  
-      sig[recoIndex-1].className += " active";
+        showSlides(recoIndex = n);
     }
